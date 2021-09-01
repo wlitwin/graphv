1c80b9a82ae621e6e1684866f81ae83d3a54688c
Walter Litwinczyk <walter@litwinczyk.com>
2021-08-31 17:58:47 -0700

    Use unsafe_set for more performance
    The bounds are already checked at a higher level, so an extra bounds
    check is unnecessary overhead.

--- renderers/gles2/native/stubs.ml
+++ renderers/gles2/native/stubs.ml
@@ -230,7 +230,7 @@ module Buffer = struct
             arr
 
         let get : t -> int -> float = Bigarray.Array1.unsafe_get
-        let set : t -> int -> float -> unit = Bigarray.Array1.set
+        let set : t -> int -> float -> unit = Bigarray.Array1.unsafe_set
         let length : t -> int  = Bigarray.Array1.dim
         let fill : t -> float -> unit  = Bigarray.Array1.fill
         let blit ~(src : t) ~(s_off : int) ~(dst : t) ~(d_off : int) ~(len : int) : unit = 
