3af50fc0b91b4dfb84527d57ef2b2bf39decf9ce
Walter Litwinczyk <walter@litwinczyk.com>
2021-10-16 21:42:40 -0700

    Add some more animation tests, many more to go

--- graphv_anim/test/graphv_anim_tests.ml
+++ graphv_anim/test/graphv_anim_tests.ml
@@ -13,18 +13,18 @@ let complete str id status =
     Printf.printf "%s %d %s\n%!" str id (string_of_status status)
 ;;
 
-let print_end_results (t : Driver.t) =
-    Printf.printf "Active %d = 0\n%!" (Driver.active_count t);
-    Printf.printf "Pending %d = 0\n%!" (Driver.active_count t);
+let print_end_results ?(expected_active=0) ?(expected_pending=0) (t : Driver.t) =
+    Printf.printf "Active %d = %d\n%!" (Driver.active_count t) expected_active;
+    Printf.printf "Pending %d = %d\n%!" (Driver.active_count t) expected_pending;
 ;;
 
-let run_simple_test (anim : anim) (ticks : float list) =
+let run_simple_test ?expected_active ?expected_pending (anim : anim) (ticks : float list) =
     let driver = Driver.create() in
     Printf.printf "Starting %d\n%!" (Driver.start driver anim);
     List.iter (fun tick ->
         Driver.tick driver tick;
     ) ticks;
-    print_end_results driver 
+    print_end_results ?expected_active ?expected_pending driver 
 ;;
 
 let basic time prefix =
@@ -358,6 +358,125 @@ let%expect_test "Basic infinite" =
       Basic update 0.75
       Basic update 1.00
       Basic complete 0 Done
+      Active 0 = 0
+      Pending 0 = 0 |}]
+;;
+
+let%expect_test "Simple parallel" =
+    let anim = parallel ~complete:(complete "Parallel") [
+        create 1. ~complete:(complete "Basic 1") (update "Basic 1");
+        create 1. ~complete:(complete "Basic 2") (update "Basic 2");
+        create 1. ~complete:(complete "Basic 3") (update "Basic 3");
+        create 1. ~complete:(complete "Basic 4") (update "Basic 4");
+    ] in
+    run_simple_test anim [0.25; 0.25; 0.25; 0.25;];
+    [%expect {|
+      Starting 0
+      Basic 1 0.25
+      Basic 2 0.25
+      Basic 3 0.25
+      Basic 4 0.25
+      Basic 1 0.50
+      Basic 2 0.50
+      Basic 3 0.50
+      Basic 4 0.50
+      Basic 1 0.75
+      Basic 2 0.75
+      Basic 3 0.75
+      Basic 4 0.75
+      Basic 1 1.00
+      Basic 1 0 Done
+      Basic 2 1.00
+      Basic 2 0 Done
+      Basic 3 1.00
+      Basic 3 0 Done
+      Basic 4 1.00
+      Basic 4 0 Done
+      Parallel 0 Done
+      Active 0 = 0
+      Pending 0 = 0 |}]
+;;
+
+let%expect_test "Nested parallel" =
+    let anim = parallel ~complete:(complete "Outer parallel") [
+        parallel ~complete:(complete "Inner parallel 1") [
+            create 1. ~complete:(complete "Basic 1") (update "Basic 1");
+            create 1. ~complete:(complete "Basic 2") (update "Basic 2");
+        ];
+        parallel ~complete:(complete "Inner parallel 2") [
+            create 1. ~complete:(complete "Basic 3") (update "Basic 3");
+            create 1. ~complete:(complete "Basic 4") (update "Basic 4");
+        ];
+    ] in
+    run_simple_test anim [0.5; 0.5];
+    [%expect {|
+      Starting 0
+      Basic 1 0.50
+      Basic 2 0.50
+      Basic 3 0.50
+      Basic 4 0.50
+      Basic 1 1.00
+      Basic 1 0 Done
+      Basic 2 1.00
+      Basic 2 0 Done
+      Basic 3 1.00
+      Basic 3 0 Done
+      Basic 4 1.00
+      Basic 4 0 Done
+      Inner parallel 1 0 Done
+      Inner parallel 2 0 Done
+      Outer parallel 0 Done
+      Active 0 = 0
+      Pending 0 = 0 |}]
+;;
+
+let%expect_test "Serial culls infinite" =
+    let anim = serial ~complete:(complete "Serial") [
+        create 1. ~complete:(complete "Basic 1") (update "Basic 1");
+        create 1. ~repeat:Infinite ~complete:(complete "Basic 2") (update "Basic 2");
+        create 1. ~complete:(complete "Basic 3") (update "Basic 3");
+    ] in
+    run_simple_test anim 
+        [0.5; 0.5; 0.5; 0.5];
+    [%expect {|
+      Starting 0
+      Basic 1 0.50
+      Basic 1 1.00
+      Basic 1 0 Done
+      Basic 2 0.00
+      Basic 2 0.50
+      Basic 2 1.00
+      Basic 2 0 Done
+      Active 0 = 0
+      Pending 0 = 0 |}]
+;;
+;;
+
+let%expect_test "Parallel does not cull infinite" =
+    let anim = parallel ~complete:(complete "Parallel") [
+        create 1. ~complete:(complete "Basic 1") (update "Basic 1");
+        create 1. ~repeat:Infinite ~complete:(complete "Basic 2") (update "Basic 2");
+        create 1. ~complete:(complete "Basic 3") (update "Basic 3");
+    ] in
+    run_simple_test anim 
+        [0.5; 0.5; 0.5; 0.5; 0.5; 0.5];
+    [%expect {|
+      Starting 0
+      Basic 1 0.50
+      Basic 2 0.50
+      Basic 3 0.50
+      Basic 1 1.00
+      Basic 1 0 Done
+      Basic 2 1.00
+      Basic 2 0 Done
+      Basic 3 1.00
+      Basic 3 0 Done
+      Basic 2 0.50
+      Basic 2 1.00
+      Basic 2 0 Done
+      Basic 2 0.50
+      Basic 2 1.00
+      Basic 2 0 Done
       Active 0 = 0
       Pending 0 = 0 |}]
 ;;
